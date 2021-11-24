abe5e2c210e859e77416d10a1e2b77ddeced74e0
Sebastian Londero <xebadevs@gmail.com>
2021-11-24 20:41:37 -0300

    'Const_elements'

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Constants
+
+const element_He = $('.element_He')
+const element_C = $('.element_C')
+const element_B = $.('element_B')
+const element_U = $.('element_U')
+const element_P = $.('element_P')
+const element_V = $.('element_V')
+const element_Ta = $.('element_Ta')

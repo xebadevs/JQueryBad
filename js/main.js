754d2f44c83c8fc3c9ed5e2a7e88061b44c1d366
Sebastian Londero <xebadevs@gmail.com>
2021-11-24 20:47:36 -0300

    'Const_buttons'

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ const element_U = $.('element_U')
 const element_P = $.('element_P')
 const element_V = $.('element_V')
 const element_Ta = $.('element_Ta')
+
+const btn_aboutus = $.('#btn_aboutus')
+const btn_products = $.('#btn_products')
+const btn_privacy = $.('#btn_privacy')
+const btn_contact = $.('#btn_contact')
+
+const iframe = $.('#iframe')
+
+const form_send = $.('#form_send')
